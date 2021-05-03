7caac417945f6ebf5901f0feb77f30fcf46d93a3
Kenjataimu <ooscaar.j@gmail.com>
2021-05-03 21:03:39 +0200

    Included schema for creating the database and the insertion of initial values in \resources Planning to use them for reset()

---
+++ src/main/resources/public/create-schema.sql
@@ -0,0 +1,72 @@
+set foreign_key_checks = 0;
+
+DROP TABLE IF EXISTS Customers;
+DROP TABLE IF EXISTS Orders;
+DROP TABLE IF EXISTS PalletCounter;
+DROP TABLE IF EXISTS Pallets;
+DROP TABLE IF EXISTS Cookies;
+DROP TABLE IF EXISTS Ingredients;
+DROP TABLE IF EXISTS Recipes;
+
+CREATE TABLE Customers
+(customerName varchar(20),
+ address varchar(20),
+ PRIMARY KEY (customerName)
+);
+
+
+CREATE TABLE Orders
+(orderNumber INT,
+ palletLabel INT,
+ orderDate DATE,
+ customer varchar(20),
+ PRIMARY KEY (orderNumber),
+ FOREIGN KEY (customer) references Customers (customerName),
+ FOREIGN KEY (palletLabel) references Pallets (palletLabel)
+);
+
+
+CREATE TABLE Pallets
+(palletLabel INT,
+ cookie varchar(20),
+ curLoc varchar(30),
+ packingDate datetime,
+ deliveryDate datetime,
+ blocked boolean,
+ PRIMARY KEY (palletLabel),
+ FOREIGN KEY (cookie) references Cookies (cookieName)
+);
+
+
+CREATE TABLE PalletCounter
+(numOfPallets INT,
+ cookie varchar(20),
+ FOREIGN KEY (cookie) references Cookies (cookieName)
+);
+
+
+CREATE TABLE Cookies
+(cookieName varchar(20),
+ PRIMARY KEY (cookieName)
+);
+
+
+CREATE TABLE Ingredients
+(ingType  varchar(60),
+ amount INT,
+ unit varchar(10),
+ delDate DATE,
+ delAmount INT,
+ PRIMARY KEY (ingType)
+);
+
+
+CREATE TABLE Recipes
+(cookie varchar(20),
+ ingType varchar(30),
+ amount INT,
+ FOREIGN KEY (cookie) references Cookies (cookieName),
+ FOREIGN KEY (ingType) references Ingredients (ingType)
+);
+
+set foreign_key_checks = 1;
---
+++ src/main/resources/public/initial-data.sql
@@ -0,0 +1,105 @@
+insert into Customers values("Bjudkakor AB", 'Ystad');
+
+insert into Customers values('Finkakor AB', 'Helsingborg');
+
+insert into Customers values('Gästkakor AB', 'Hässleholm');
+
+insert into Customers values('Kaffebröd AB', 'Landskrona');
+
+insert into Customers values('Kalaskakor AB', 'Trelleborg');
+
+insert into Customers values('Partykakor AB', 'Kristianstad');
+
+insert into Customers values('Skånekakor AB', 'Perstorp');
+
+insert into Customers values('Småbröd AB', 'Malmö');
+
+insert into Cookies values('Almond delight');
+
+insert into Cookies values('Amneris');
+
+insert into Cookies values('Berliner');
+
+insert into Cookies values('Nut cookie');
+
+insert into Cookies values('Nut ring');
+
+insert into Cookies values('Tango');
+
+insert into Ingredients values ('Bread crumbs', 500000, 'g', '2021-05-03', 500000);
+
+insert into Ingredients values ('Butter', 500000, 'g', '2021-05-03', 500000);
+
+insert into Ingredients values ('Chocolate', 500000, 'g', '2021-05-03', 500000);
+
+insert into Ingredients values ('Chopped almonds', 500000, 'g', '2021-05-03', 500000);
+
+insert into Ingredients values ('Cinnamon', 500000, 'g', '2021-05-03', 500000);
+
+insert into Ingredients values ('Egg whites', 500000, 'ml', '2021-05-03', 500000);
+
+insert into Ingredients values ('Fine-ground nuts', 500000, 'g', '2021-05-03', 500000);
+
+insert into Ingredients values ('Ground, roasted nuts', 500000, 'g', '2021-05-03', 500000);
+
+insert into Ingredients values ('Icing sugar', 500000, 'g', '2021-05-03', 500000);
+
+insert into Ingredients values ('Marzipan', 500000, 'g', '2021-05-03', 500000);
+
+insert into Ingredients values ('Potato starch', 500000, 'g', '2021-05-03', 500000);
+
+insert into Ingredients values ('Roasted, chopped nuts', 500000, 'g', '2021-05-03', 500000);
+
+insert into Ingredients values ('Sodium bicarbonate', 500000, 'g', '2021-05-03', 500000);
+
+insert into Ingredients values ('Sugar', 500000, 'g', '2021-05-03', 500000);
+
+insert into Ingredients values ('Flour', 500000, 'g', '2021-05-03', 500000);
+
+insert into Ingredients values ('Eggs', 500000, 'g', '2021-05-03', 500000);
+
+insert into Ingredients values ('Vanilla sugar', 500000, 'g', '2021-05-03', 500000);
+
+insert into Ingredients values ('Vanilla', 500000, 'g', '2021-05-03', 500000);
+
+insert into Ingredients values ('Wheat flour', 500000, 'g', '2021-05-03', 500000);
+
+insert into Recipes values('Almond delight', 'Butter', 400);
+insert into Recipes values('Almond delight', 'Chopped almonds', 279);
+insert into Recipes values('Almond delight', 'Cinnamon', 10);
+insert into Recipes values('Almond delight', 'Flour', 400);
+insert into Recipes values('Almond delight', 'Sugar', 270);
+
+insert into Recipes values('Amneris', 'Butter', 250);
+insert into Recipes values('Amneris', 'Eggs', 250);
+insert into Recipes values('Amneris', 'Marzipan', 750);
+insert into Recipes values('Amneris', 'Potato starch', 25);
+insert into Recipes values('Amneris', 'Wheat flour', 25);
+
+insert into Recipes values('Berliner ', 'Butter', 250);
+insert into Recipes values('Berliner ', 'Chocolate', 50);
+insert into Recipes values('Berliner ', 'Eggs', 50);
+insert into Recipes values('Berliner ', 'Flour', 350);
+insert into Recipes values('Berliner ', 'Icing sugar', 100);
+insert into Recipes values('Berliner ', 'Vanilla sugar', 5);
+
+
+insert into Recipes values ('Nut cookie', 'Bread crumbs', 125);
+insert into Recipes values ('Nut cookie', 'Chocolate', 50);
+insert into Recipes values ('Nut cookie', 'Egg whites', 350);
+insert into Recipes values ('Nut cookie', 'Fine-ground nuts', 750);
+insert into Recipes values ('Nut cookie', 'Ground, roasted nuts', 625);
+insert into Recipes values ('Nut cookie', 'Sugar', 375);
+
+insert into Recipes values ('Nut ring', 'Butter', 450);
+insert into Recipes values ('Nut ring', 'Flour', 450);
+insert into Recipes values ('Nut ring', 'Icing sugar', 190);
+insert into Recipes values ('Nut ring', 'Roasted, chopped nuts', 225);
+
+insert into Recipes values ('Tango', 'Butter', 200);
+insert into Recipes values ('Tango', 'Flour', 300);
+insert into Recipes values ('Tango', 'Sodium bicarbonate', 4);
+insert into Recipes values ('Tango', 'Sugar', 250);
+insert into Recipes values ('Tango', 'Vanilla', 2);
+
+TRUNCATE TABLE Pallets;
